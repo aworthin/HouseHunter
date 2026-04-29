f740da4e39b813a3a4332f3e11ae7b159d4de576
HomeQuest Deploy <deploy@househunter.app>
2026-04-29 14:53:49 +0000

    Move Firebase config to environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZfpReWfQzt2RTQ39dPb0CukJE1u7e6Yc",
-  authDomain: "bw-house-hunter.firebaseapp.com",
-  projectId: "bw-house-hunter",
-  storageBucket: "bw-house-hunter.firebasestorage.app",
-  messagingSenderId: "880335295884",
-  appId: "1:880335295884:web:b754a4b87db2f3247772a9",
-  measurementId: "G-CCJPMTPKYE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
